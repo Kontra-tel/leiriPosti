0f4cdd2631a56f5b8fdbd5282d5fd2b3d3b692f2
markus <mag3768@gmail.com>
2025-04-27 16:50:49 +0300

    app testing base

--- app/src/main/java/tel/kontra/leiriposti/App.java
+++ app/src/main/java/tel/kontra/leiriposti/App.java
@@ -1,5 +1,13 @@
 package tel.kontra.leiriposti;
 
+import java.awt.print.PrinterException;
+
+import javax.print.PrintService;
+
+import tel.kontra.leiriposti.controller.PrinterController;
+import tel.kontra.leiriposti.controller.SheetsController;
+import tel.kontra.leiriposti.model.Message;
+import tel.kontra.leiriposti.model.PrintersNotFoundException;
 import tel.kontra.leiriposti.view.MainGui;
 
 public class App {
@@ -7,10 +15,24 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello, Leiriposti!");
 
-        //String sheetId = "1SPtpixQ5uZGS9u6u5XOfk6WWgvRTNsSvggl63GdR_zA";
+        // String sheetId = "1YvVGb-Zs83-ar92oJeyDbgPqyHcLe3BUDI6tQdGZYDc";
 
-        // Launch the GUI        
-        MainGui.launch(MainGui.class, args);
+        // SheetsController sc = new SheetsController(sheetId);
+        // PrinterController pc = new PrinterController();
+
+        // // Print all printservices
+        // PrintService[] printServices = pc.getPrintServices();
+
+        // for (PrintService printService : printServices) {
+        //     System.out.println(printService.getName());
+        // }
+        // 
+        // Message msg = sc.getMessage(3);
+
+        // System.out.println(msg);
+
+         //Launch the GUI        
+         MainGui.launch(MainGui.class, args);
 
     }
 }
