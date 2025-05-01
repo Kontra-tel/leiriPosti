c653c9d8ee9fa7cf0c401d601056ce8e4e8c1447
markus <mag3768@gmail.com>
2025-05-01 13:43:30 +0300

    refactor: uncomment and reorganize main method for improved readability and functionality

--- app/src/main/java/tel/kontra/leiriposti/App.java
+++ app/src/main/java/tel/kontra/leiriposti/App.java
@@ -15,24 +15,40 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello, Leiriposti!");
 
-        // String sheetId = "1YvVGb-Zs83-ar92oJeyDbgPqyHcLe3BUDI6tQdGZYDc";
+        String sheetId = "1YvVGb-Zs83-ar92oJeyDbgPqyHcLe3BUDI6tQdGZYDc";
 
-        // SheetsController sc = new SheetsController(sheetId);
-        // PrinterController pc = new PrinterController();
+        SheetsController sc = new SheetsController(sheetId);
+        PrinterController pc = new PrinterController();
 
-        // // Print all printservices
-        // PrintService[] printServices = pc.getPrintServices();
+        // Print all printservices
+        PrintService[] printServices = pc.getPrintServices();
 
-        // for (PrintService printService : printServices) {
-        //     System.out.println(printService.getName());
-        // }
-        // 
-        // Message msg = sc.getMessage(3);
+        for (PrintService printService : printServices) {
+            System.out.println(printService.getName());
+        }
+        
+        Message msg = sc.getMessage(2);
 
-        // System.out.println(msg);
+        System.out.println(msg);
 
-         //Launch the GUI        
-         MainGui.launch(MainGui.class, args);
+        // Set printer to last one in the list if the array is not empty
+        if (printServices.length > 0) {
+            pc.setDefaultPrintService(printServices[printServices.length - 1]);
+        } else {
+            System.err.println("No print services available.");
+        }
+
+        // Print the message
+        try {
+            pc.sendToPrinter(msg);
+        } catch (PrintersNotFoundException e) {
+            System.err.println("No printers found: " + e.getMessage());
+        } catch (PrinterException e) {
+            System.err.println("Printer error: " + e.getMessage());
+        }
+
+        //Launch the GUI        
+        //MainGui.launch(MainGui.class, args);
 
     }
 }
