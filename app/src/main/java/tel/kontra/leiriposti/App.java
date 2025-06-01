f056833dd03d5faa0677a141416d9525bd3b1506
markus <mag3768@gmail.com>
2025-06-01 09:58:26 +0300

    feat: enable debug mode in application using command line argument

--- app/src/main/java/tel/kontra/leiriposti/App.java
+++ app/src/main/java/tel/kontra/leiriposti/App.java
@@ -2,6 +2,7 @@ package tel.kontra.leiriposti;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
 
 import tel.kontra.leiriposti.view.MainGui;
 
@@ -11,6 +12,14 @@ public class App {
 
     public static void main(String[] args) {
         LOGGER.info("Hello, Leiriposti!");
+
+        // If the application is run with the --debug flag, enable console
+        if( args.length > 0 && args[0].equals("--debug")) {
+            Configurator.setRootLevel(org.apache.logging.log4j.Level.DEBUG);
+            LOGGER.debug("Debug mode enabled.");
+        } else {
+            Configurator.setRootLevel(org.apache.logging.log4j.Level.INFO);
+        }
 
         //Launch the GUI        
         MainGui.launch(MainGui.class, args);
